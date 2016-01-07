a38003d343ec0cf4f44935a2361569f490e36f74
Lukas Martinelli <me@lukasmartinelli.ch>
2016-01-07 20:03:42 +0100

    Specify git_commit variable

--- linter/utils.sh
+++ linter/utils.sh
@@ -3,9 +3,16 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+readonly GIT_COMMIT=${GIT_COMMIT:-}
+
 function clone_repo() {
     local working_dir=$(mktemp -dt "lint.XXX")
     local git_output=$(git clone --quiet "$GIT_REPOSITORY" "$working_dir")
+
+    if [ ! -z "$GIT_COMMIT" ]; then
+        (cd "$working_dir" && git checkout "$GIT_COMMIT" > /dev/null 2>&1)
+    fi
+
     echo "$working_dir"
 }
 
